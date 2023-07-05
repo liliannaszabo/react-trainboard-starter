328caf203331b02687aff6bceb0825cb9ed40075
Lilianna Szabo <lilianna.szabo@softwire.com>
2023-07-05 11:37:48 +0100

    Switched to react.select

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Select  from 'react-select';
 import Button from './Button';
 
 type DropdownProps = {
     onChange: (option: React.SetStateAction<string> ) => void;
 }
+
+interface StationOption {
+    value: string;
+    label: string;
+}
+
 const DropdownMenu: React.FC<DropdownProps>  = ({ onChange }) => {
     const [selectedOption, setSelectedOption] = useState('');
-    const handleSelectChange = (event: { target: { value: React.SetStateAction<string>}}) => {
-        const option = event.target.value;
-        setSelectedOption(option);
-        onChange(option);
+    const handleSelectChange = (option?: StationOption | null | undefined) => {
+        if(option){
+            console.log(`value ${option.value} label ${option.label}`);
+            setSelectedOption(option.value);
+            onChange(option.value);
+        }
     };
+
+    const stations = [
+        { label: 'Kings Cross', value: 'KGX' },
+        { label: 'Cambridge', value: 'CBG' },
+        { label: 'Stevenage', value: 'SVG' },
+        { label: 'Edinburgh Waverley' , value: 'EDB' },
+        { label: 'Nottingham', value: 'NOT' },
+    ];
 
     return (
         <div>
             <label htmlFor = "dropdown">Select an option:</label>
-            <select id = "dropdown" value = { selectedOption } onChange = { handleSelectChange }>
-                <option value = "">Select</option>
-                <option value = "KGX">Kings Cross</option>
-                <option value = "CBG">Cambridge</option>
-                <option value = "SVG">Stevenage</option>
-                <option value = "EDB">Edinburgh Waverley</option>
-                <option value = "NOT">Nottingham</option>
-            </select>
+            <Select options = { stations } 
+                onChange = { (option: StationOption | null | undefined) =>  handleSelectChange(option) }/>
         </div>
     );
 };
